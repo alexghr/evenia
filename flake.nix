c365717d38747b8d9df9c61b533258e773a8b2ef
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-07-22 10:41:51 +0100

    build: add prisma

--- flake.nix
+++ flake.nix
@@ -12,14 +12,15 @@
           inherit system;
           overlays = [ corepack.overlays.default ];
         };
+        mysqlUser = "mysql";
         # remote host running MySQL (MariaDB)
-        mysqlHost = "mysql@b1";
+        mysqlHost = "b1";
         localSocket = "/tmp/mysql.sock";
         remoteSocket = "/var/run/mysqld/mysqld.sock";
         # wrapper shell script to use local socket
         mysql = pkgs.writeShellScriptBin "mysql" ''
           #!/usr/bin/env bash
-          ${pkgs.mysql}/bin/mysql --socket=${localSocket} "$@"
+          ${pkgs.mysql}/bin/mysql --socket=${localSocket} --user ${mysqlUser} "$@"
         '';
       in
         {
@@ -27,12 +28,21 @@
             buildInputs = [
               pkgs.nodejs-18_x
               pkgs.corepack
+              pkgs.prisma-engines
               mysql
             ];
 
+            PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+            PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+            PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+            PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
+            PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+
+            DATABASE_URL = "mysql://${mysqlUser}@localhost/events?socket=${localSocket}";
+
             shellHook = ''
               trap 'pkill -f ${localSocket}; rm ${localSocket}' EXIT
-              ${pkgs.openssh}/bin/ssh -N -f -L ${localSocket}:${remoteSocket} ${mysqlHost}
+              ${pkgs.openssh}/bin/ssh -N -f -L ${localSocket}:${remoteSocket} ${mysqlUser}@${mysqlHost}
             '';
           };
         }
